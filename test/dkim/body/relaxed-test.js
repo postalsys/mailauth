e0edf6b277d0211c236f596ffc878372d315cd44
Andris Reinman <andris@kreata.ee>
2020-08-22 11:45:59 +0300

    somewhat working signing

--- test/dkim/body/relaxed-test.js
+++ test/dkim/body/relaxed-test.js
@@ -1,7 +1,7 @@
 /* eslint no-unused-expressions:0 */
 'use strict';
 
-const { resolveStream } = require('../../../lib/tools');
+const { writeToStream } = require('../../../lib/tools');
 const chai = require('chai');
 const expect = chai.expect;
 
@@ -15,69 +15,43 @@ describe('DKIM RelaxedBody Tests', () => {
         const message = Buffer.from('\r\n\r\n\n\r\n\r\n');
 
         let s = new RelaxedBody({
-            hashAlgo: 'sha256'
+            algorithm: 'sha256'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        await resolveStream(s, message, 1);
-        expect(hash).to.equal('47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=');
+        await writeToStream(s, message, 1);
+        expect(s.bodyHash).to.equal('47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=');
     });
 
     it('Should calculate sha1 body hash for an empty message', async () => {
         const message = Buffer.from('\r\n\r\n\n\r\n\r\n');
 
         let s = new RelaxedBody({
-            hashAlgo: 'sha1'
+            algorithm: 'sha1'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        await resolveStream(s, message, 1);
-        expect(hash).to.equal('2jmj7l5rSw0yVb/vlWAYkK/YBwk=');
+        await writeToStream(s, message, 1);
+        expect(s.bodyHash).to.equal('2jmj7l5rSw0yVb/vlWAYkK/YBwk=');
     });
 
     it('Should calculate body hash byte by byte', async () => {
         let message = await fs.readFile(__dirname + '/../../fixtures/message1.eml');
 
         let s = new RelaxedBody({
-            hashAlgo: 'sha256'
+            algorithm: 'sha256'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        s.on('headers', d => console.log(d));
-
-        await resolveStream(s, message, 1);
-        expect(hash).to.equal('D2H5TEwtUgM2u8Ew0gG6vnt/Na6L+Zep7apmSmfy8IQ=');
+        await writeToStream(s, message, 1);
+        expect(s.bodyHash).to.equal('D2H5TEwtUgM2u8Ew0gG6vnt/Na6L+Zep7apmSmfy8IQ=');
     });
 
     it('Should calculate body hash all at once', async () => {
         let message = await fs.readFile(__dirname + '/../../fixtures/message1.eml');
 
         let s = new RelaxedBody({
-            hashAlgo: 'sha256'
+            algorithm: 'sha256'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        await resolveStream(s, message, 1000000);
-        expect(hash).to.equal('D2H5TEwtUgM2u8Ew0gG6vnt/Na6L+Zep7apmSmfy8IQ=');
+        await writeToStream(s, message, 1000000);
+        expect(s.bodyHash).to.equal('D2H5TEwtUgM2u8Ew0gG6vnt/Na6L+Zep7apmSmfy8IQ=');
     });
 });
--- test/dkim/body/simple-test.js
+++ test/dkim/body/simple-test.js
@@ -1,7 +1,8 @@
 /* eslint no-unused-expressions:0 */
 'use strict';
 
-const { resolveStream } = require('../../../lib/tools');
+const { writeToStream } = require('../../../lib/tools');
+
 const chai = require('chai');
 const expect = chai.expect;
 
@@ -15,53 +16,33 @@ describe('DKIM SimpleBody Tests', () => {
         const message = Buffer.from('\r\n\r\n\n\r\n\r\n');
 
         let s = new SimpleBody({
-            hashAlgo: 'sha256'
+            algorithm: 'sha256'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        await resolveStream(s, message, 1);
-        expect(hash).to.equal('frcCV1k9oG9oKj3dpUqdJg1PxRT2RSN/XKdLCPjaYaY=');
+        await writeToStream(s, message, 1);
+        expect(s.bodyHash).to.equal('frcCV1k9oG9oKj3dpUqdJg1PxRT2RSN/XKdLCPjaYaY=');
     });
 
     it('Should calculate sha1 body hash for an empty message', async () => {
         const message = Buffer.from('\r\n\r\n\n\r\n\r\n');
 
         let s = new SimpleBody({
-            hashAlgo: 'sha1'
+            algorithm: 'sha1'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        await resolveStream(s, message, 1);
-        expect(hash).to.equal('uoq1oCgLlTqpdDX/iUbLy7J1Wic=');
+        await writeToStream(s, message, 1);
+        expect(s.bodyHash).to.equal('uoq1oCgLlTqpdDX/iUbLy7J1Wic=');
     });
 
     it('Should calculate body hash byte by byte', async () => {
         let message = await fs.readFile(__dirname + '/../../fixtures/message1.eml');
 
         let s = new SimpleBody({
-            hashAlgo: 'sha256'
+            algorithm: 'sha256'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        s.on('headers', d => console.log(d));
-
-        await resolveStream(s, message, 1);
-        expect(hash).to.equal('ESwKBtV2kJ5cP058Rw3B6BZLVaL2SNau6GDddaItvi4=');
+        await writeToStream(s, message, 1);
+        expect(s.bodyHash).to.equal('ESwKBtV2kJ5cP058Rw3B6BZLVaL2SNau6GDddaItvi4=');
     });
 
     it('Should calculate body hash all at once', async () => {
@@ -70,16 +51,10 @@ describe('DKIM SimpleBody Tests', () => {
         message = Buffer.from(message.toString().replace(/\r?\n/g, '\r\n'));
 
         let s = new SimpleBody({
-            hashAlgo: 'sha256'
+            algorithm: 'sha256'
         });
 
-        let hash;
-        // 'hash' is triggered before stream end
-        s.on('hash', h => {
-            hash = h;
-        });
-
-        await resolveStream(s, message, 1000000);
-        expect(hash).to.equal('ESwKBtV2kJ5cP058Rw3B6BZLVaL2SNau6GDddaItvi4=');
+        await writeToStream(s, message, 1000000);
+        expect(s.bodyHash).to.equal('ESwKBtV2kJ5cP058Rw3B6BZLVaL2SNau6GDddaItvi4=');
     });
 });
