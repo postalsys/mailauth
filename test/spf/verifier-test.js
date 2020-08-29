fa7e5151f729851ff6412c327ad916f78ef68ae6
Andris Reinman <andris@kreata.ee>
2020-08-29 11:22:26 +0300

    updated dns resolver handling

--- test/spf/verifier-test.js
+++ test/spf/verifier-test.js
@@ -10,79 +10,89 @@ chai.config.includeStack = true;
 
 describe('SPF Verifier Tests', () => {
     it('Should pass all IPs', async () => {
-        let resolver = {
-            resolveTxt: () => [['v=spf1 +all']]
-        };
+        const stubResolver = () => [['v=spf1 +all']];
 
-        let result = await spfVerify('example.com', { ip: '1.2.3.4', resolver });
+        let result = await spfVerify('example.com', { ip: '1.2.3.4', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
     });
 
     it('Should fail all IPs', async () => {
-        let resolver = {
-            resolveTxt: () => [['v=spf1 -all']]
-        };
+        const stubResolver = () => [['v=spf1 -all']];
 
-        let result = await spfVerify('example.com', { ip: '1.2.3.4', resolver });
+        let result = await spfVerify('example.com', { ip: '1.2.3.4', resolver: stubResolver });
         expect(result.qualifier).to.equal('-');
     });
 
     it('Should pass A records', async () => {
-        let resolver = {
-            resolveTxt: () => [['v=spf1 a -all']],
-            resolve4: () => ['192.0.2.10', '192.0.2.11']
+        const stubResolver = (domain, type) => {
+            switch (type) {
+                case 'TXT':
+                    return [['v=spf1 a -all']];
+                case 'A':
+                    return ['192.0.2.10', '192.0.2.11'];
+            }
         };
 
         let result;
 
-        result = await spfVerify('example.com', { ip: '1.2.3.4', resolver });
+        result = await spfVerify('example.com', { ip: '1.2.3.4', resolver: stubResolver });
         expect(result.qualifier).to.equal('-');
 
-        result = await spfVerify('example.com', { ip: '192.0.2.10', resolver });
+        result = await spfVerify('example.com', { ip: '192.0.2.10', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
 
-        result = await spfVerify('example.com', { ip: '192.0.2.11', resolver });
+        result = await spfVerify('example.com', { ip: '192.0.2.11', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
     });
 
     it('Should pass MX records', async () => {
-        let resolver = {
-            resolveTxt: () => [['v=spf1 mx -all']],
-            resolve4: () => ['192.0.2.10', '192.0.2.11'],
-            resolveMx: () => [{ priority: 1, exchange: 'example.com' }]
+        const stubResolver = (domain, type) => {
+            switch (type) {
+                case 'TXT':
+                    return [['v=spf1 mx -all']];
+                case 'A':
+                    return ['192.0.2.10', '192.0.2.11'];
+                case 'MX':
+                    return [{ priority: 1, exchange: 'example.com' }];
+            }
         };
 
         let result;
 
-        result = await spfVerify('example.com', { ip: '1.2.3.4', resolver });
+        result = await spfVerify('example.com', { ip: '1.2.3.4', resolver: stubResolver });
         expect(result.qualifier).to.equal('-');
 
-        result = await spfVerify('example.com', { ip: '192.0.2.10', resolver });
+        result = await spfVerify('example.com', { ip: '192.0.2.10', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
 
-        result = await spfVerify('example.com', { ip: '192.0.2.11', resolver });
+        result = await spfVerify('example.com', { ip: '192.0.2.11', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
     });
 
     it('Should pass MX/CIDR records', async () => {
-        let resolver = {
-            resolveTxt: () => [['v=spf1 mx/24 -all']],
-            resolve4: () => ['192.0.2.10', '192.0.2.11'],
-            resolveMx: () => [{ priority: 1, exchange: 'example.com' }]
+        const stubResolver = (domain, type) => {
+            switch (type) {
+                case 'TXT':
+                    return [['v=spf1 mx/24 -all']];
+                case 'A':
+                    return ['192.0.2.10', '192.0.2.11'];
+                case 'MX':
+                    return [{ priority: 1, exchange: 'example.com' }];
+            }
         };
 
         let result;
 
-        result = await spfVerify('example.com', { ip: '1.2.3.4', resolver });
+        result = await spfVerify('example.com', { ip: '1.2.3.4', resolver: stubResolver });
         expect(result.qualifier).to.equal('-');
 
-        result = await spfVerify('example.com', { ip: '192.0.2.10', resolver });
+        result = await spfVerify('example.com', { ip: '192.0.2.10', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
 
-        result = await spfVerify('example.com', { ip: '192.0.2.11', resolver });
+        result = await spfVerify('example.com', { ip: '192.0.2.11', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
 
-        result = await spfVerify('example.com', { ip: '192.0.2.56', resolver });
+        result = await spfVerify('example.com', { ip: '192.0.2.56', resolver: stubResolver });
         expect(result.qualifier).to.equal('+');
     });
 });
