8b1062773f56375896db60b7c5b95ff3c5faaa00
Andris Reinman <andris@kreata.ee>
2020-10-29 11:42:39 +0200

    started with arc-message-signature signing

--- examples/authenticate.js
+++ examples/authenticate.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const { authenticate } = require('../lib/mailauth');
+const { dkimSign } = require('../lib/dkim/sign');
+
 const fs = require('fs');
 
 const main = async () => {
-    let message = fs.createReadStream(process.argv[2] || __dirname + '/../test/fixtures/message4.eml');
+    let message = await fs.promises.readFile(process.argv[2] || __dirname + '/../test/fixtures/message4.eml');
     let res = await authenticate(message, {
         ip: '217.146.67.33',
         helo: 'uvn-67-33.tll01.zonevs.eu',
@@ -16,6 +18,19 @@ const main = async () => {
     console.log('----');
     console.log(res.headers.trim());
     console.log('----');
+
+    let signed = await dkimSign(message, {
+        signTime: Date.now(),
+        arc: {
+            instance: 1,
+            algorithm: 'rsa-sha256',
+            signingDomain: 'tahvel.info',
+            selector: 'test.rsa',
+            privateKey: fs.readFileSync('./test/fixtures/private-rsa.pem')
+        }
+    });
+
+    console.log(signed);
 };
 
 main()
