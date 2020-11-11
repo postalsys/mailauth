4ee9d65ac8473f37696b3f440ce108303aafc967
Andris Reinman <andris@kreata.ee>
2020-11-11 15:14:37 +0200

    Added simple tests for BIMI resolving

---
+++ test/bimi-test.js
@@ -0,0 +1,114 @@
+/* eslint no-unused-expressions:0 */
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+let { bimi } = require('../lib/bimi');
+
+chai.config.includeStack = true;
+
+// NB! these tests perform live DNS and HTTPS queries
+
+const dnsReject = () => {
+    let err = new Error('Error');
+    err.code = 'ENOTFOUND';
+    throw err;
+};
+
+describe('BIMI Tests', () => {
+    it('Should resolve BIMI location', async () => {
+        let res = await bimi({
+            dmarc: {
+                status: {
+                    result: 'pass',
+                    header: {
+                        from: 'gmail.com'
+                    }
+                },
+                domain: 'gmail.com',
+                policy: 'reject'
+            },
+            resolver: async (name, rr) => {
+                if (rr !== 'TXT') {
+                    dnsReject();
+                }
+                switch (name) {
+                    case 'default._bimi.gmail.com':
+                        return [['v=BIMI1; l=https:', '//cldup.com/a6t0ORNG2z.svg']];
+                    default: {
+                        dnsReject();
+                    }
+                }
+            }
+        });
+
+        expect(res?.status?.result).to.equal('pass');
+        expect(res?.status?.header).to.deep.equal({ selector: 'default', d: 'gmail.com' });
+        expect(res?.location).to.equal('https://cldup.com/a6t0ORNG2z.svg');
+    });
+
+    it('Should resolve author BIMI location', async () => {
+        let res = await bimi({
+            dmarc: {
+                status: {
+                    result: 'pass',
+                    header: {
+                        from: 'sub.gmail.com'
+                    }
+                },
+                domain: 'gmail.com',
+                policy: 'reject'
+            },
+            resolver: async (name, rr) => {
+                if (rr !== 'TXT') {
+                    dnsReject();
+                }
+                switch (name) {
+                    case 'default._bimi.sub.gmail.com':
+                        return [['v=BIMI1; l=https:', '//cldup.com/a6t0ORNG2y.svg']];
+                    case 'default._bimi.gmail.com':
+                        return [['v=BIMI1; l=https:', '//cldup.com/a6t0ORNG2z.svg']];
+                    default: {
+                        dnsReject();
+                    }
+                }
+            }
+        });
+
+        expect(res?.status?.result).to.equal('pass');
+        expect(res?.status?.header).to.deep.equal({ selector: 'default', d: 'sub.gmail.com' });
+        expect(res?.location).to.equal('https://cldup.com/a6t0ORNG2y.svg');
+    });
+
+    it('Should resolve organization BIMI location', async () => {
+        let res = await bimi({
+            dmarc: {
+                status: {
+                    result: 'pass',
+                    header: {
+                        from: 'sub.gmail.com'
+                    }
+                },
+                domain: 'gmail.com',
+                policy: 'reject'
+            },
+            resolver: async (name, rr) => {
+                if (rr !== 'TXT') {
+                    dnsReject();
+                }
+                switch (name) {
+                    case 'default._bimi.gmail.com':
+                        return [['v=BIMI1; l=https:', '//cldup.com/a6t0ORNG2z.svg']];
+                    default: {
+                        dnsReject();
+                    }
+                }
+            }
+        });
+
+        expect(res?.status?.result).to.equal('pass');
+        expect(res?.status?.header).to.deep.equal({ selector: 'default', d: 'gmail.com' });
+        expect(res?.location).to.equal('https://cldup.com/a6t0ORNG2z.svg');
+    });
+});
