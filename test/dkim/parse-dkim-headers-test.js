d28663b30b0bfaf07d395e9d3eaea044c9085657
Andris Reinman <andris@reinman.eu>
2024-01-24 13:41:25 +0200

    fix(dkim-verify): Show the length of the source body in DKIM results

---
+++ test/dkim/parse-dkim-headers-test.js
@@ -0,0 +1,20 @@
+/* eslint no-unused-expressions:0 */
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+let parseDkimHeaders = require('../../lib/parse-dkim-headers');
+
+chai.config.includeStack = true;
+
+describe('parseDkimHeaders Tests', () => {
+    it('Should parse ARC header', () => {
+        console.log(
+            parseDkimHeaders(
+                'i=1; mx.microsoft.com 1; spf=fail (sender ip is 52.138.216.130) smtp.rcpttodomain=recipient.com smtp.mailfrom=sender.com; dmarc=fail (p=reject sp=reject pct=100) action=oreject header.from=sender.com; dkim=none (message not signed); arc=none (0)'
+            )
+        );
+        expect(1).to.exist;
+    });
+});
